d5af321b7c6d325f0e98b7789004feff544f4f49
Lakhan Gupta <lakhangupta53223@gmail.com>
2024-06-03 23:41:54 +0530

    this is for ngModel new concept

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MycomponentComponent } from './mycomponent/mycomponent.component';
@@ -14,7 +14,8 @@ import { ShivaComponentComponent } from './shiva-component/shiva-component.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule ,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
